3864757623dbad6101bc985d288062fb82818fbb
Woerly Camille <dukk@macbook-pro-de-woerly.home>
2023-10-20 16:38:11 +0200

    a bit of style polish & add marvel logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FlexBox, Alignment } from '@lumx/react';
 import Search from '../Search';
+const marvelLogo = require("../../images/marvel.png");
 
 const Header = ({inputValue, setInputValue}) => (
 	<header className="lumx-spacing-padding-big header">
+		<img className="marvel_logo" src={marvelLogo} alt="marvel logo on header"/>	
 		<FlexBox vAlign={Alignment.right}>
 			<Search inputValue={inputValue} setInputValue={setInputValue} />
 		</FlexBox>
